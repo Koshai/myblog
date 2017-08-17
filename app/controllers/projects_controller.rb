549cb539f196de85094dd064249593177641f791
Syed Rubayyat Akbar <syed.r.akbar@gmail.com>
2017-08-17 05:19:48 -0400

    A slight problem solving

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+  def index
+    @projects = Project.all.order("created_at desc")
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+
+    if @project.save
+      redirect_to @project, notice: "Woot! Your project has been saved!"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project, notice: "Woot! Your project has been updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  def find_project
+    @project = Project.friendly.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :link, :slug)
+  end
+end
